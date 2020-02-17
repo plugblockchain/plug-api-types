f6d5c571bf19a42c1e125014238f3ed33e04042b
Jordan Beauchamp <beauchjord@gmail.com>
2020-02-18 12:56:58 +1300

    Remove build artefacts

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Doughnut from './Doughnut';
-import PlugExtrinsicV1 from './Extrinsic';
-declare const PlugRuntimeTypes: {
-    Doughnut: typeof Doughnut;
-    ExtrinsicV4: typeof PlugExtrinsicV1;
-    RewardBalance: string;
-};
-export default PlugRuntimeTypes;
