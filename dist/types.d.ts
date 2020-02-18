0004ccde6c2fc33df8579f9e35d7ed94080aeede
Jordan Beauchamp <beauchjord@gmail.com>
2020-02-18 20:37:59 +1300

    Ignore dist

--- dist/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { u128 } from '@polkadot/types/primitive';
-import { CompactEncodable } from '@polkadot/types/codec/Compact';
-export interface Balance extends u128, CompactEncodable {
-    toNumber(): number;
-}
--- dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const primitive_1 = require("@polkadot/types/primitive");
-{
-    primitive_1.u128.toNumber();
-}
